8fef0ca3a159ef9a68e88c5598c4cd2f719902a2
Thorn Walli <thorn.walli@grabarzundpartner.de>
2016-09-24 16:58:44 +0200

    fix

--- src/performance/stats.js
+++ src/performance/stats.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var script = require('../../script');
+var script = require('../script');
 
 if((isDevMode() && !isStatsDisabled()) || (!isDevMode() && isStatsEnabled())) {
     loadStatsScript();
